2854fa5fce5fbe7d12ac1eb18710afdc5d120117
Leonie Dong <leoniedong@hotmail.com>
2019-04-10 23:24:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   lowest_key = ""
   lowest_number = ""
   name_hash.each do |name, number|
-    if lowest_number = "" || number < lowest_number
+    if lowest_number == "" || number < lowest_number
       lowest_number = number
       lowest_key = name
     end
