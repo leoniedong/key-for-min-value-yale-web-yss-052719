64d62de9f78a5968759e130c89b63ca71d4c256a
Leonie Dong <leoniedong@hotmail.com>
2019-04-10 23:21:22 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  lowest_key = ""
+  lowest_pair = ""
   name_hash.each do |name, number|
-    
+    if
   end
 end
